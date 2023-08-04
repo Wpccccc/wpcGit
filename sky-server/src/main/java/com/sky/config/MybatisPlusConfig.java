e5d4020992f49f1a2293d9989b7b698a4cf996e2
wpc <2596884995@qq.com>
2023-08-04 15:27:04 +0800

    苍穹外卖V1.1 员工分页查询

---
+++ sky-server/src/main/java/com/sky/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.sky.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wpc
+ * @date 2023/8/4 13:34
+ */
+
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
